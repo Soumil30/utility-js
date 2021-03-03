89c0dc4cc72ff9f43d3a4846eb6b10b17dbab41a
Soumil <soumilde30@gmail.com>
2021-03-04 00:51:34 +0530

    Added test for filter function passed with non-empty array
    Modified filter.js to work for all input cases.
    Successfully passed test case for filtering a non-empty array.

--- src/filter.js
+++ src/filter.js
@@ -1,8 +1,15 @@
+const head = require('./head');
+const tail = require('./tail');
+
 const filter = (collection, func) => {
     if (collection.length == 0) {
         return collection;
     }
 
-    return null;
+    var filteredArray = filter(tail(collection), func);
+    if (func(head(collection))) {
+        filteredArray.unshift(head(collection));
+    }
+    return filteredArray;
 }
 module.exports = filter;
--- src/filter.spec.js
+++ src/filter.spec.js
@@ -5,4 +5,8 @@ describe('Filter', () => {
     it('filter([], x => true) should give "[]"', () => {
         expect(filter([], x => true)).toEqual([]);
     });
+
+    it('filter([1,2,3], x => true) should give "[1,2,3]"', () => {
+        expect(filter([1,2,3], x => true)).toEqual([1,2,3]);
+    });
 })
