aa8cae8a2eab40bc24af44a14d7c8fcd2211af41
Soumil <soumilde30@gmail.com>
2021-03-03 21:08:47 +0530

    Added test for getting tail of an empty array
    Test case for empty array successfully passed.

--- tail.spec.js
+++ tail.spec.js
@@ -5,4 +5,8 @@ describe('Tail', () => {
     it(`tail([1,2,3]) should be "[2,3]"`, () => {
         expect(tail([1,2,3])).toEqual([2,3]);
     });
+
+    it(`tail([]) should be "[]"`, () => {
+        expect(tail([])).toEqual([]);
+    })
 })
