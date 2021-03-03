de420ac0c587555f2f74d68ba11248d44dc6c79b
Soumil <soumilde30@gmail.com>
2021-03-03 23:44:35 +0530

    Added test for using map with cube function on an array
    Successfully passed test case for mapping cube function on non-empty array.

--- src/map.spec.js
+++ src/map.spec.js
@@ -11,4 +11,8 @@ describe('Map', () => {
     it(`map([1,2,3], identity) should give "[1,2,3]"`, () => {
         expect(map([1,2,3], identity)).toEqual([1,2,3]);
     });
+
+    it(`map([1,2,3], cube) should give "[1,8,27]"`, () => {
+        expect(map([1,2,3], cube)).toEqual([1,8,27]);
+    });
 })
