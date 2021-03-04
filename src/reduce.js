0d4edd76613445a82c47076be4fd7c5d57fe7458
Soumil <soumilde30@gmail.com>
2021-03-04 22:58:13 +0530

    Added test for reduce function passed with array of strings
    Modified reduce.js to work for non-empty arrays.
    Successfully passed test case for reducing array of strings.

--- src/reduce.js
+++ src/reduce.js
@@ -1,8 +1,14 @@
+const head = require('./head');
+const tail = require('./tail');
+
 const reduce = (collection, func, currentValue) => {
     if (collection.length == 0) {
         return currentValue;
     }
-
-    return null;
+    if (currentValue != undefined) {
+        return reduce(tail(collection), func, func(currentValue, head(collection)));
+    }
+    
+    return reduce(tail(collection), func, head(collection));
 }
 module.exports = reduce;
--- src/reduce.spec.js
+++ src/reduce.spec.js
@@ -6,7 +6,11 @@ describe('Reduce', () => {
         expect(reduce([], (x,y) => x + y)).toEqual(undefined);
     });
 
-    it('reduce([],v(x,y)=>x+y,10) should be "10"', () => {
+    it('reduce([],(x,y)=>x+y,10) should be "10"', () => {
         expect(reduce([], (x,y) => x + y, 10)).toEqual(10);
+    });
+
+    it("reduce(['a','b','c'],(x,y)=>x+y) should be `abc`", () => {
+        expect(reduce(['a','b','c'], (x,y) => x + y)).toEqual('abc');
     });
 })
